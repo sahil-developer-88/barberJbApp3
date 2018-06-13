4b19dcc28981f881b8b02c02e6b4c3ae29ba99b7
sahil-developer-88 <sahiljain12345@gmail.com>
2018-06-13 19:12:49 +0530

    update package and server file

--- server.js
+++ server.js
@@ -3,22 +3,48 @@ const express = require('express');
 const path = require('path');
 const http = require('http');
 const bodyParser = require('body-parser');
-
+const passport=require('passport');
+const passportSetup=require('./server/database/passport.config');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const dbConnection = require('./server/database/models/connection');
 // Get our API routes
-const api = require('./server/routes/api');
+// const api = require('./server/routes/api');
 
 const app = express();
 
 
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 // var app = express();
-var cookieParser = require('cookie-parser');
-var session = require('express-session');
+
+
 app.use(cookieParser());
 app.use(session({
     secret: 'sessionTesting',
-    saveUninitialized: false,
-    resave: false
+    store: new MongoStore({
+        mongooseConnection: dbConnection,
+        ttl: 3600 * 24 * 60, // 2 months
+        touchAfter: 3600 * 24 * 2
+    }),
+    unset: 'destroy',
+    cookie: { maxAge: (3600000 * 24 * 60) }, // 2 months
+    resave: false,
+    saveUninitialized: true
 })); 
+
+
+// passport session initialization
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Parsers for POST data
 app.use(bodyParser.json());
@@ -28,7 +54,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Set our api routes
-app.use('/api', api);
+// app.use('/api', api);
+
+var routes = require('./server/routes/router')(app);
+app.use(routes);
 
 // Catch all other routes and return the index file
 app.get('*', (req, res) => {
